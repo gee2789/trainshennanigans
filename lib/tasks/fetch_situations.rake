e7e2eb5782919456eb9fc8598a84c929a3d8a64c
gee2789 <geeyung.li@gmail.com>
2017-07-10 18:56:17 -0400

    implemented initial tests

--- lib/tasks/fetch_situations.rake
+++ lib/tasks/fetch_situations.rake
@@ -1,7 +1,8 @@
 require 'SituationFetcher'
 
 namespace :situation do
+  desc "pull information from XML MTA details"
   task :fetch do
-    
+
   end
 end
--- spec/models/situation_spec.rb
+++ spec/models/situation_spec.rb
@@ -2,24 +2,5 @@ require 'rails_helper'
 
 describe Situation do
   describe '#create' do
-    context 'when XML file is valid' do
-      let(:xml) {
-        doc = open(Rails.root.join("spec","fixtures","ServiceStatusSubway.xml"))
-        Crack::XML.parse(doc)
-      }
-      let(:situation) {
-        create(:situation, raw_)
-      }
-
-      before do
-
-      end
-
-      it "properly saves the situation time" do
-
-      end
-    end
-    context 'when XML file is not valid' do
-    end
   end
 end
